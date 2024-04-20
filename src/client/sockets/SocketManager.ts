1230441615e172a69877895a0f9dc14f196d2d00
Migue <mmoyano16@gmial.com>
2024-04-19 22:42:43 -0300

    fix: server url

--- src/client/sockets/SocketManager.ts
+++ src/client/sockets/SocketManager.ts
@@ -3,6 +3,6 @@ import io, { Socket } from "socket.io-client";
 export class SocketManager {
   private socket: Socket;
   constructor() {
-    this.socket = io("localhost:3000");
+    this.socket = io();
   }
 }
