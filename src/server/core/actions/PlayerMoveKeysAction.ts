5aa67814c084ff6db121695ebdca95b86f8edb51
migue <mmoyano16@gmail.com>
2024-05-05 15:10:49 -0300

    fix: position

--- src/server/core/actions/PlayerMoveKeysAction.ts
+++ src/server/core/actions/PlayerMoveKeysAction.ts
@@ -19,8 +19,8 @@ export class PlayerMoveKeysAction implements Action<PlayerMoveKeysActionProps, v
   public execute(input: PlayerMoveKeysActionProps): void {
     const player = this.gameState.players[input.playerId];
     if (!player) return;
-    const dx = input.keys.right ? 6 : input.keys.left ? -6 : 0;
-    const dy = input.keys.down ? 6 : input.keys.up ? -6 : 0;
+    const dx = input.keys.right ? 16 : input.keys.left ? -16 : 0;
+    const dy = input.keys.down ? 16 : input.keys.up ? -16 : 0;
     if (dx === 0 && dy === 0) return;
     player.setTargetPosition({
       x: player.playerState.position.x + dx,
